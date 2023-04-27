6283f9e441b9c4ee31bc4c7be76261ba52d31bd5
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-04-27 12:47:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,77 +1,21 @@
 ﻿const int NUMBER_OF_STONES = 1_000_000;
-const int MIN_RANDOM = 1;
-const int MAX_RANDOM = 1_000_000_000 + 1;
+const int MIN_VALUE = 1;
+const int MAX_VALUE = 1_000_000_000 + 1;
 
-var randomNumbers = GenerateRandomArray();
-// Array.Sort(randomNumbers);
-QuickSort(ref randomNumbers, 0, NUMBER_OF_STONES - 1);
-double averageDistane = CalculateAverageDistance(randomNumbers);
-Console.WriteLine($"average distance: {averageDistane:n5}");
+Console.WriteLine($"average distance: {GetAverageDistance():n5}");
 
-int[] GenerateRandomArray()
+double GetAverageDistance()
 {
     var rand = new Random(4711);
-    var randomNumbers = new int[NUMBER_OF_STONES];
+    double min = MAX_VALUE, max = MIN_VALUE, ran;
+
     for (int i = 0; i < NUMBER_OF_STONES; i++)
     {
-        // randomNumbers[i] = Random.Shared.Next(MIN_RANDOM, MAX_RANDOM);
-        randomNumbers[i] = rand.Next(MIN_RANDOM, MAX_RANDOM);
-    }
-    return randomNumbers;
-}
-
-double CalculateAverageDistance(int[] numbers)
-{
-    var distances = new int[NUMBER_OF_STONES - 1];
-    for (int i = 0; i < NUMBER_OF_STONES - 1; i++)
-    {
-        distances[i] = numbers[i + 1] - numbers[i];
-    }
-    return distances.Average();
-}
-
-void BubbleSort(ref int[] numbers)
-{
-    for (int i = 0; i < NUMBER_OF_STONES - 1; i++)
-    {
-        for (int j = 0; j < NUMBER_OF_STONES - 1; j++) //++ erhöht die Variable
-        {
-            if (numbers[j] > numbers[j + 1])
-            {
-                (numbers[j], numbers[j + 1]) = (numbers[j + 1], numbers[j]);
-            }
-        }
-    }
-}
-
-void QuickSort(ref int[] numbers, int left, int right)
-{
-    if (left < right)
-    {
-        int partitionIndex = Partition(ref numbers, left, right);
-        QuickSort(ref numbers, left, partitionIndex - 1);
-        QuickSort(ref numbers, partitionIndex + 1, right);
-    }
-}
-
-int Partition(ref int[] numbers, int left, int right)
-{
-    int pivot = numbers[right];
-    int i = left - 1;
-
-    for (int j = left; j < right; j++)
-    {
-        if (numbers[j] <= pivot)
-        {
-            i++;
-            (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
-        }
+        // ran = Random.Shared.Next(MIN_VALUE, MAX_VALUE);
+        ran = rand.Next(MIN_VALUE, MAX_VALUE);
+        if (ran < min) { min = ran; }
+        if (ran > max) { max = ran; }
     }
 
-    (numbers[i + 1], numbers[right]) = (numbers[right], numbers[i + 1]);
-    return i + 1;
-
+    return (max - min) / (NUMBER_OF_STONES - 1);
 }
-
-
-
