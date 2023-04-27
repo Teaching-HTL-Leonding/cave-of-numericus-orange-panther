acb82463c437e9355e0ff95cde58e663dbac0d5a
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-04-27 10:42:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿const int NUMBER_OF_STONES = 20;
+var randomNumbers = GenerateRandomArray();
+Array.Sort(randomNumbers);
+double averageDistane = CalculateAverageDistance(randomNumbers);
+Console.WriteLine($"average distance: {averageDistane}");
+
+int[] GenerateRandomArray()
+{
+    //var rand = new Random(4711);
+    var randomNumbers = new int[20];
+    for (int i = 0; i < NUMBER_OF_STONES; i++)
+    {
+        randomNumbers[i] = Random.Shared.Next(1, 101);
+        //randomNumbers[i] = rand.Next(1, 101);
+    }
+    return randomNumbers;
+}
+
+double CalculateAverageDistance(int[] numbers)
+{
+    var distances = new int[numbers.Length - 1];
+    for (int i = 0; i < numbers.Length - 1; i++)
+    {
+        distances[i] = numbers[i + 1] - numbers[i];
+    }
+    return distances.Average();
+}
+
