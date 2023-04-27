cd75ab0b8be3621d756676161937d7b28733f421
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-04-27 11:00:58 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿const int NUMBER_OF_STONES = 75000;
 const int MIN_RANDOM = 1;
-const int MAX_RANDOM = 100 + 1;
+const int MAX_RANDOM = 1_000_000_000 + 1;
 
 var randomNumbers = GenerateRandomArray();
 Array.Sort(randomNumbers);
 double averageDistane = CalculateAverageDistance(randomNumbers);
-Console.WriteLine($"average distance: {averageDistane}");
+Console.WriteLine($"average distance: {averageDistane:n5}");
 
 int[] GenerateRandomArray()
 {
     //var rand = new Random(4711);
-    var randomNumbers = new int[20];
+    var randomNumbers = new int[NUMBER_OF_STONES];
     for (int i = 0; i < NUMBER_OF_STONES; i++)
     {
         randomNumbers[i] = Random.Shared.Next(MIN_RANDOM, MAX_RANDOM);
